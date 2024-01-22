9b68234ece03cc997ecefcd56ed8c85631b4694a
benny <bennycortese@gmail.com>
2024-01-22 15:44:53 -0500

    Routes now rendering properly by not having the default page render on each component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App_output.css';
+import AboutMe from './AboutMe';
 
-function App() {
-  return (
-    
-    
-    <div className="App">
+// Placeholder components for demonstration
+const Projects: React.FC = () => <div><h1>Projects Page</h1></div>;
+const Resume: React.FC = () => <div><h1>Resume Page</h1></div>;
+const Home: React.FC = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>Hello! I am Benny Cortese! Welcome to my Shenanigans!</p>
+  </header>
+);
 
-    <div className="navbar bg-base-100">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">Benny Cortese</a>
-        </div>
-        <div className="flex-none">
+const App: React.FC = () => {
+    return (
+      <Router>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <Link to="/" className="btn btn-ghost text-xl">Benny Cortese</Link>
+          </div>
+          <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
-                <li><a>Projects</a></li>
-                <li><a>Resume</a></li>
-                <li><a>About Me</a></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/aboutme">About Me</Link></li> {/* Ensure URL is lowercase */}
             </ul>
+          </div>
         </div>
-    </div>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! I am Benny Cortese! Welcome to my Schenanigans!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
-  );
+  
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/aboutme" element={<AboutMe />} /> {/* Ensure URL is lowercase */}
+        </Routes>
+      </Router>
+    );
 }
 
 export default App;
