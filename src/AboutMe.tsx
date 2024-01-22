6edf56a603d258c179d5113174f2f324532cd5a2
benny <bennycortese@gmail.com>
2024-01-22 17:07:22 -0500

    Changed the background color, fixed some image sizes, still needs a crazy amount of work but things are starting to click

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -12,18 +12,15 @@ const AboutMe: React.FC = () => {
 const [currentImage, setCurrentImage] = useState<string>(blastoise); // Default to the first image
 
   return (
-    <div>
-      <h1>Drawings and Doodles</h1>
 
-      <div className="avatar">
-        <div className="w-32 rounded">
-          <img src={currentImage} alt="Displayed" className="w-full h-auto" />
-        </div>
-      </div>
+    <div style={{ backgroundColor: '#FAF9F6' }} className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Drawings and Doodles</h1>
+
+      <img src={currentImage} alt="Selected Art" className="rounded-lg mb-4" />
 
 
       <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">Click</div>
+          <div tabIndex={0} role="button" className="btn m-1">Select an option</div>
           <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
               <li><a onClick={() => setCurrentImage(blastoise)}>Blastoise</a></li>
               <li><a onClick={() => setCurrentImage(bart_simpson)}>Bart Simpson</a></li>
