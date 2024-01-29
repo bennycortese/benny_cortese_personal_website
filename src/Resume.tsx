8e1f896578ca602d058332e9e7642a26a43d4362
benny <bennycortese@gmail.com>
2024-01-28 23:59:11 -0500

    Added changes to resume formatting page

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,24 +1,20 @@
 
 import React, {useState} from 'react';
+import { Document, Page } from 'react-pdf';
 
 const Resume: React.FC = () => {
-const [currentImage, setCurrentImage] = useState<string>(); // Default to the first image
+    const pdfURL = 'path_to_pdf_file.pdf'; // need to add a link to my resume pdf here
+    const [currentImage, setCurrentImage] = useState<string>(); // Default to the first image
 
   return (
     
     <div style={{ backgroundColor: '#FAF9F6' }} className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Drawings and Doodles</h1>
-
-      <img src={currentImage} alt="Selected Art" className="rounded-lg mb-4" />
-
-
-      <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">Select an option</div>
-          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-          </ul>
-        </div>
+      <h1 className="text-2xl font-bold mb-4">Hello! My name is Benny Cortese. Linked here is my resume.</h1>
+      <Document file={pdfURL}>
+        <Page pageNumber={1} />
+        </Document>
     </div>
-    
+    //download button here
   );
 }
 
