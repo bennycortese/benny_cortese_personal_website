29c669d351a7bfdd7fe4e7e92669838342faf194
Benny Cortese <bennycortese@gmail.com>
2024-01-31 23:56:06 -0500

    Update Resume.tsx to add better text

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -8,7 +8,7 @@ const Resume: React.FC = () => {
   return (
     
     <div style={{ backgroundColor: '#FAF9F6' }} className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Hello! My name is Benny Cortese.</h1>
+      <h1 className="text-2xl font-bold mb-4">Hello! My name is Benny Cortese. This is my Resume!</h1>
       <div>
         
       </div>
