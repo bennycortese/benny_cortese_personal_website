8a8496239583db7613ba45a61e345304124a1cf3
benny <bennycortese@gmail.com>
2024-02-05 00:00:05 -0500

    Added an index page for image exporting

---
+++ src/art_index.tsx
@@ -0,0 +1,18 @@
+// benny_art/index.ts
+import blastoise from './blastoise1.png';
+import bart_simpson from './bart_simpson1.png';
+import gumball from './gumball1.png';
+import jack_skellington from './jack_skellington1.png';
+import pearls_rat from './pearls_rat1.png';
+import snoopy from './snoopy1.png';
+import voltorb from './voltorb1.png';
+
+export default {
+  blastoise,
+  bart_simpson,
+  gumball,
+  jack_skellington,
+  pearls_rat,
+  snoopy,
+  voltorb,
+};
