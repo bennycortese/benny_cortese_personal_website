1379d0d94ec09e7541bc3a8c47b1cc403f901e58
benny <bennycortese@gmail.com>
2024-02-11 23:58:59 -0500

    Fixed import problem in art_index

--- src/art_index.tsx
+++ src/art_index.tsx
@@ -1,11 +1,11 @@
 // art_index.tsx
-import blastoise from './blastoise1.png';
-import bart_simpson from './bart_simpson1.png';
-import gumball from './gumball1.png';
-import jack_skellington from './jack_skellington1.png';
-import pearls_rat from './pearls_rat1.png';
-import snoopy from './snoopy1.png';
-import voltorb from './voltorb1.png';
+import blastoise from './benny_art/blastoise1.png';
+import bart_simpson from './benny_art/bart_simpson1.png';
+import gumball from './benny_art/gumball1.png';
+import jack_skellington from './benny_art/jack_skellington1.png';
+import pearls_rat from './benny_art/pearls_rat1.png';
+import snoopy from './benny_art/snoopy1.png';
+import voltorb from './benny_art/voltorb1.png';
 // figure out why this is broken
 export default {
   blastoise,
