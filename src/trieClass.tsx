1a78261d20d7610863ba0f4737b7a56e374e9931
benny <bennycortese@gmail.com>
2024-02-17 23:58:56 -0500

    Trie Class fixed for typescript semantics

--- src/trieClass.tsx
+++ src/trieClass.tsx
@@ -1,53 +1,62 @@
-class TrieNode {
-  constructor() {
-    this.children = {};
-    this.isEndOfWord = false;
-  }
-}
-
-class Trie {
-  constructor() {
-    this.root = new TrieNode();
-  }
-
-  insert(word) {
-    let node = this.root;
-    for (let i = 0; i < word.length; i++) {
-      let char = word[i];
-      if (!node.children[char]) {
-        node.children[char] = new TrieNode();
-      }
-      node = node.children[char];
+type TrieNodeChildren = {
+    [key: string]: TrieNode;
+  };
+  
+  class TrieNode {
+    children: TrieNodeChildren;
+    isEndOfWord: boolean;
+  
+    constructor() {
+      this.children = {};
+      this.isEndOfWord = false;
     }
-    node.isEndOfWord = true;
   }
-
-  search(word) {
-    let node = this.root;
-
-    for (let i = 0; i < word.length; i++) {
-      let char = word[i];
-
-      if (!node.children[char]) {
-        return false;
-      }
-      node = node.children[char];
+  
+  class Trie {
+    root: TrieNode;
+  
+    constructor() {
+      this.root = new TrieNode();
     }
-    return node.isEndOfWord;
-  }
-
-  startsWith(prefix) {
-    let node = this.root;
-    for (let i = 0; i < prefix.length; i++) {
-      let char = prefix[i];
-      if (!node.children[char]) {
-        return false;
+  
+    insert(word: string): void {
+      let node = this.root;
+      for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (!node.children[char]) {
+          node.children[char] = new TrieNode();
+        }
+        node = node.children[char];
       }
-      node = node.children[char];
+      node.isEndOfWord = true;
     }
-    return true;
+  
+    search(word: string): boolean {
+      let node = this.root;
+  
+      for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (!node.children[char]) {
+          return false;
+        }
+        node = node.children[char];
+      }
+      return node.isEndOfWord;
+    }
+  
+    startsWith(prefix: string): boolean {
+      let node = this.root;
+      for (let i = 0; i < prefix.length; i++) {
+        const char = prefix[i];
+        if (!node.children[char]) {
+          return false;
+        }
+        node = node.children[char];
+      }
+      return true;
+    }
   }
-}
-
-// Optional export
-module.exports = Trie;
+  
+  // Optional export
+  export default Trie;
+  
