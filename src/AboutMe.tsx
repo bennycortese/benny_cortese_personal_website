d3a6cc282a862966f2ee22f3d935527597ac4201
bennycortese <bennycortese@gmail.com>
2025-04-29 23:36:51 -0700

    Updates to the logos and styling on the page, added momentic

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -11,14 +11,23 @@ const AboutMe: React.FC = () => {
 
       <div className="text-center">
         <h1 className="text-3xl font-bold mt-0">Hi, I am Benny Cortese!</h1>
-        <p className="text-xl mt-2">Software Engineer Intern at Figma, passionate about building awesome things with technology!</p>
+        <p className="text-xl mt-2">I am passionate about building awesome things with technology!</p>
       </div>
         
 
       {/* Professional Experience Section -- definitely add images */}
       <div className="my-4">
         <h2 className="text-2xl font-bold text-center mb-2">Professional Experience</h2>
+        <div className="grid grid-cols-3 gap-4 mb-2">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg" alt="Microsoft" className="mx-auto w-32 h-32"/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" alt="Figma" className="mx-auto w-32 h-32"/>
+          <img src="https://media.licdn.com/dms/image/v2/D560BAQGMuyaxxew9AA/company-logo_200_200/company-logo_200_200/0/1727939535273/momentic_ai_logo?e=1751500800&v=beta&t=540UdsEqBF_WlJHq4cklVswtaphIu6MG3o4oKPIEgfc" alt="Momentic" className="mx-auto w-32 h-32"/>
+          <img src="https://media.licdn.com/dms/image/v2/C560BAQHpvZB-7BigTg/company-logo_200_200/company-logo_200_200/0/1631417945298/kpcb_fellows_program_logo?e=1751500800&v=beta&t=kpjkKJSZoRcSX3akskvZjUZJhPkiuOi7h0hKVptltHY" alt="KP fellow" className="mx-auto w-32 h-32"/>
+          <img src="https://media.licdn.com/dms/image/v2/C560BAQGZOQ8pLmLpDQ/company-logo_200_200/company-logo_200_200/0/1630665521597/neoventures_logo?e=1751500800&v=beta&t=tVJTReQRyAxCoGtgb-RlPea_ilv-uaRH0pTOQOfeChU" alt="Neo" className="mx-auto w-32 h-32"/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Y_Combinator_logo.svg/1200px-Y_Combinator_logo.svg.png" alt="YCombinator" className="mx-auto w-32 h-32"/>
+        </div>
         <ul className="list-disc pl-5">
+          <li> Founding Engineer at Momentic, 2nd Engineer hired, YC S24 (January 2024 - April 2024)</li>
           <li>Kleiner Perkins Engineering Fellow (May 2024 - Present)</li>
           <li>Software Engineering Intern at Figma on Figjam AI and AI Productivity teams (May 2024 - August 2024)</li>
           <li>Neo Scholar Finalist (Jul 2023 - Present)</li>
@@ -27,11 +36,9 @@ const AboutMe: React.FC = () => {
         </ul>
       </div>
 
-
-
       {/* Skills Section */}
       <div className="">
-        <h2 className="text-2xl font-bold text-center mb-1">Skills</h2>
+        <h2 className="text-2xl font-bold text-center mb-1 mt-2">Skills</h2>
         <div className="grid grid-cols-3 gap-4">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" alt="C++" className="mx-auto w-32 h-32"/>
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" alt="Python" className="mx-auto w-32 h-32"/>
